0ef152d7fe3b852c69352c8abdd831af19c97ecf
Matteo Bertucci <matteobertucci2004@gmail.com>
2022-02-11 10:20:18 +0000

    CLI setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+mod logging;
+mod cli;
+
+extern crate clap;
+
+use log::{debug, warn, LevelFilter};
+use clap::Parser;
+
+use logging::init_cli_logging;
+use cli::Cli;
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    // Start logging in debug if the verbose flag is present.
+    init_cli_logging(
+        if cli.verbose {LevelFilter::Debug} else {LevelFilter::Info}
+    ).expect("Failed to initialize logging.");
+
+    debug!("Logging successfully initialized.");
+    #[cfg(debug_assertions)]
+    warn!("Debug build. Do not use in production.");
 }
